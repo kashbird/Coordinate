18d8055e080e27105617f754570d386bf0321e8d
kashbird <artisthg122@gmail.com>
2022-04-12 15:19:34 +0900

    haha

---
+++ index.js
@@ -0,0 +1,28 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        
+        // vertical.style.transform = `translateX(${변수}px)`
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+        tag.innerHTML = `${x}px, ${y}px`;
+        
+        if(x > 1000) {
+            tag.style.opacity = '50%';
+        } else {
+            tag.style.opacity = '100%';
+        }
+    })
+});
